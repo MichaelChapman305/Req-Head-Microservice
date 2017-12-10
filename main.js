188faaac646abee7705f087d8ff6559f9cb23f11
Michael A. Chapman <chapmanm305@gmail.com>
2017-12-10 18:10:08 -0500

    adding port for heroku

--- main.js
+++ main.js
@@ -8,6 +8,4 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => {
-  console.log('This app is running on Localhost:3000');
-});
+app.listen(process.env.PORT || 5000);
