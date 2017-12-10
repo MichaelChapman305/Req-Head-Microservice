6e6d29f3ecf704da4be15a792f4b301dc25c5fff
Michael A. Chapman <chapmanm305@gmail.com>
2017-12-10 18:43:42 -0500

    Changing index of slice in software

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
   let ip = req.headers['x-forwarded-for'];
   let lang = req.headers['accept-language'].slice(0, 5);
-  let software = req.headers['user-agent'].slice(13, 40);
+  let software = req.headers['user-agent'].slice(req.headers['user-agent'].indexOf('(') + 1, req.headers['user-agent'].indexOf(')'));
   console.log(req.headers);
   res.render('index', { ip, lang, software });
 });
