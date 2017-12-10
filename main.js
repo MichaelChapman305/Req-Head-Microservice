750539fbab609ecc43bceaa33c3fa423d12c0cbd
Michael A. Chapman <chapmanm305@gmail.com>
2017-12-10 18:34:06 -0500

    adding req.headers for ip, language, and software

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'pug');
-
+  
 app.get('/', (req, res) => {
-  console.log(req.connection.remoteAddress);
-  res.render('index');
+  let ip = req.headers['x-forwarded-for'];
+  let lang = req.headers['accept-language'].slice(0, 5);
+  let software = req.headers['user-agent'].slice(13, 40);
+  console.log(req.headers);
+  res.render('index', { ip, lang, software });
 });
 
 app.listen(process.env.PORT || 5000);
