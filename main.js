9835aaf56f70e5e56b3c4513d0e4d5c16a2a1899
Michael A. Chapman <chapmanm305@gmail.com>
2017-12-10 19:20:26 -0500

    getting rid of slices in change of split for language request header

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ app.set('view engine', 'pug');
   
 app.get('/', (req, res) => {
   let ip = req.headers['x-forwarded-for'];
-  let lang = req.headers['accept-language'].slice(0, 5);
+  let lang = req.headers['accept-language'].split(',')[0];
   let software = req.headers['user-agent'].slice(req.headers['user-agent'].indexOf('(') + 1, req.headers['user-agent'].indexOf(')'));
   console.log(req.headers);
   res.render('index', { ip, lang, software });
